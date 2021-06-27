3b2bbe827dcba120723f4444354c4945cb3b0fd5
Abigail Limpioso <alimpioso@gmail.com>
2021-06-27 00:45:26 -0400

    added addition section

--- react-calculator/src/components/Addition/index.js
+++ react-calculator/src/components/Addition/index.js
@@ -1,20 +1,44 @@
 import "../../App.css";
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 //on focus of addition values, adds values and shows answer to the right
 export default function Addition() {
 
     const addVal1 = useRef();
     const addVal2 = useRef();
+
+    const [solution, setSolution] = useState(0);
+
+    //adds all values
+    const add = (e) => {
+
+        e.preventDefault();
+        console.log('past')
+        if(parseFloat(addVal1.current.value) > 1 && parseFloat(addVal2.current.value) > 1) {
+            setSolution(parseFloat(addVal1.current.value) + parseFloat(addVal2.current.value));
+        }
+    
+    }
+
+    //resets form
+    const reset = (e) => {
+
+        e.preventDefault();
+        addVal1.current.value = 0;
+        addVal2.current.value = 0;
+        setSolution(0);
+
+    }
     
     return (
         <div className="add-section">
             <h2>Addition</h2>
             <form>
-                <input type="text" value="0" ref={addVal1} />
+                <input className="reset-btn" type="submit" onClick={reset} value="Reset"/>
+                <input type="text" placeholder="0" ref={addVal1} onChange={add}/>
                 <p>+</p>
-                <input type="text" value="0" ref={addVal2}/>
-                <p>= <span id="add-answer"></span></p>
+                <input type="text" placeholder="0" ref={addVal2} onChange={add}/>
+                <p>= <span id="add-answer">{solution}</span></p>
             </form>
         </div>
     )
